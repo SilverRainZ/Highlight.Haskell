f123179677a041cc709ad9f01bdf2ab6e601dda9
谷月轩 <LastAvengers@outlook.com>
2015-02-15 22:02:24 +0800

    BlkCmt doesn't work

--- highligth.hs
+++ highligth.hs
@@ -1,17 +1,35 @@
+import System.IO
+import Text.Regex.Posix
 import qualified Data.Map as Map
 import qualified Control.Monad as Monad
+type Pattern = String
+type Color = String
+
 {- Highlighter in Haskell -}
 main :: IO()
 main = do 
-        contents <- readFile "keywords.txt"
-        let kw = consKeyWord contents
-        print kw
-        Monad.forever $ do 
-                x <- getLine
-                print $ Map.lookup x kw
+        putStrLn "Reading config..."
+        handle <- openFile "keywords.txt" ReadMode
+        pattern <- sequence $ replicate 6 (hGetLine handle)
+        color <- sequence $ replicate 6 (hGetLine handle)
+        let patCol = zip pattern color
+        mapM_ print pattern
+        hClose handle
 
-pWords :: String -> [String]
-pWords = words
+        putStrLn "Processing..."
+        contents <- readFile "test.java"
+        writeFile "test.java.html" $ htmlen contents patCol
+        putStrLn "Finished."
 
-consKeyWord :: String -> Map.Map String String
-consKeyWord = Map.fromList . map (\key -> (key, "<strong>")) . lines
+htmlHead = "<!DOCTYPE html><head><meta charset=\"utf-8\"></head>"
+htmlStyle = "<style>code{font: 14px/200% \"Consolas\";}</style><body><pre><code>"
+htmlLast= "</code></pre></body></html>"
+htmlen:: String -> [(Pattern, Color)] -> String
+htmlen str patCol = let htmlBody = foldl (\acc patcol -> parser acc patcol) str patCol
+                        in htmlHead ++ htmlStyle ++ htmlBody ++ htmlLast
+
+parser :: String -> (Pattern, Color) -> String
+parser code patCol@(pattern, color) 
+    | code =~ pattern == True 
+            = (\(_head, str, _tail) -> _head ++ "<font color=" ++ color ++ ">" ++ str ++ "</font>"++ parser _tail patCol) (code =~ pattern)
+    | otherwise = code 
---
+++ test.java
@@ -0,0 +1,17 @@
+package Default;
+
+import javax.swing.*;
+
+/*
+ * Block Comment
+ */
+public static void main(String args){
+    // Line Comment
+    int a;
+    char b = 'a';
+    String str1 = new String("Who cares?");
+    String str2 = new String("Who\" cares?");
+    String str1 = new String("Who \\"cares?");
+    a = a + b;
+    return 0;
+}
