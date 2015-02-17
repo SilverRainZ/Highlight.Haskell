e53075b8f03ab8224efe984fad20b8a3d4e3bc44
谷月轩 <LastAvengers@outlook.com>
2015-02-17 15:16:31 +0800

    little change.

--- highligth.hs
+++ highligth.hs
@@ -17,14 +17,14 @@ main = do
 htmlHead = "<!DOCTYPE html><head><meta charset=\"utf-8\"></head>"
 htmlStyle = "<style>code{font: 14px/200% \"Consolas\";}</style><body><pre><code>"
 htmlLast= "</code></pre></body></html>"
-htmlen:: String -> [(Pattern, Color)] -> String
-htmlen str patCol = let htmlBody = foldl (\acc x-> parser acc x) str patCol
+htmlen :: String -> [(Pattern, Color)] -> String
+htmlen str patCol = let htmlBody = foldl rep str patCol
                         in htmlHead ++ htmlStyle ++ htmlBody ++ htmlLast
 
-parser :: String -> (Pattern, Color) -> String
-parser code patCol@(pattern, color) 
+rep :: String -> (Pattern, Color) -> String
+rep code patCol@(pattern, color) 
     | code =~ pattern == True 
-            = (\(_head, x, _tail) -> _head ++ ls ++ x ++ le ++ parser _tail patCol) (code =~ pattern)
+            = (\(_head, x, _tail) -> _head ++ ls ++ x ++ le ++ rep _tail patCol) (code =~ pattern)
     | otherwise = code 
     where ls = "<font color=" ++ color ++ ">"
           le = "</font>"
@@ -32,5 +32,6 @@ parser code patCol@(pattern, color)
 pattern' = [ "/\\*(.|\n|\r)*\\*/"
            , "//.*"
            , "#.*"
-           , "\"([^\"]|\\\")*\""]
+           , "\"(.|(\\\\\\\\)*\\\\\")*[^\\\\](\\\\\\\\)*\""
+           ]
 color = ["Purple", "Gray", "Red", "Green", "Blue"]
